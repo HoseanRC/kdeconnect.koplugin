56a7434843f777bc5a09025eb458f5cdc36389a4
HoseanRC <hoseandawoodi90@outlook.com>
2026-07-06 00:07:06 +0330

    forcezero instead of freeing the cert to make it compatible with android

--- genkey.c
+++ genkey.c
@@ -110,8 +110,7 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    wc_ecc_free(&key);
-
+    wc_ForceZero(&cert, sizeof(Cert));
     wc_ecc_free(&key);
     wc_FreeRng(&rng);
     return 0;
