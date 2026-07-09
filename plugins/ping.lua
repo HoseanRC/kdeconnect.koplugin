969d046da5cc7b83bba0764f3d5f994980e152fc
HoseanRC <hoseandawoodi90@outlook.com>
2026-07-09 18:13:46 +0330

    added ping plugin

---
+++ plugins/ping.lua
@@ -0,0 +1,28 @@
+-- Ping plugin for KDE Connect
+-- This plugin handles ping requests from connected devices
+
+local Plugin = require("./plugins/__init__").Plugin
+local UIManager = require("ui/uimanager")
+local Notification = require("ui/widget/notification")
+local _ = require("gettext")
+
+--- Ping plugin handler
+---@param plugin Plugin Plugin instance
+---@param packet table Incoming packet
+---@param device Device
+local function ping_handler(plugin, packet, device)
+  -- Display ping message on screen
+  local display_text = "Ping from " .. (device and (device.deviceName or "UNKNOWN") or "UNKNOWN")
+  UIManager:show(Notification:new {
+    text = _(display_text),
+  })
+end
+
+--- Create and return the ping plugin
+---@return Plugin
+return Plugin:new(
+  "kdeconnect.ping",
+  "Ping",
+  "both",
+  ping_handler
+)
